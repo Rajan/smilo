dd2c7a2f2fbdbdddbad82fe058a90170f2a16e8c
Rajan Chandi <chandi.rajan@gmail.com>
2015-10-14 15:02:29 +0530

    external dependency on images removed. more images added.

--- client/smilo.js
+++ client/smilo.js
@@ -1,53 +1,73 @@
 /**
  * Templates
  */
+
+Template.registerHelper('equals',
+    function (v1, v2) {
+
+        //alert("v1: " + v1 + " v2: " + v2);
+        return (v1 == v2);
+
+        /*var result = v1.localeCompare(v2);
+        if(result === 0){
+            return true;
+        }else{
+            return false;
+        }*/
+        //return true;
+    }
+);
+
 if (Meteor.isClient) {
     Template.messages.helpers({
-        messages: function() {
-            return Messages.find({}, { sort: { time: 1}});
+        messages: function () {
+            return Messages.find({}, {sort: {time: 1}});
         }
     });
 
+
+
+    function messagePosted(){
+        if (Meteor.user())
+            name = Meteor.user().profile.name;
+        else
+            name = 'Anonymous';
+        var message = document.getElementById('message');
+
+        if (message.value != '') {
+            Messages.insert({
+                name: name,
+                message: message.value,
+                time: Date.now(),
+                from: "c"
+            });
+            Meteor.call('clientMessage', message.value);
+            document.getElementById('message').value = '';
+            message.value = '';
+        }
+    }
+
+    Template.messages.events = {
+        'click .server': function (event) {
+            //var paragraph = event.currentTarget; // always a P
+            event.preventDefault();
+            console.log("You clicked a message.");
+            var clickedElement = event.target; // could be the P or a child element
+            alert(clickedElement);
+        }
+    };
+
     Template.input.events = {
-        'keydown input#message' : function (event) {
+        'keydown input#message': function (event) {
             if (event.which == 13) { // 13 is the enter key event
-                if (Meteor.user())
-                    var name = Meteor.user().profile.name;
-                else
-                    var name = 'Anonymous';
-                var message = document.getElementById('message');
-                if (message.value != '') {
-                    Messages.insert({
-                        name: name,
-                        message: message.value,
-                        time: Date.now()
-                    });
-
-                    document.getElementById('message').value = '';
-                    message.value = '';
-                }
+                messagePosted();
             }
         },
-        'click div#send' : function (event) {
-            if (Meteor.user())
-                name = Meteor.user().profile.name;
-            else
-                name = 'Anonymous';
-            var message = document.getElementById('message');
-
-            if (message.value != '') {
-                Messages.insert({
-                    name: name,
-                    message: message.value,
-                    time: Date.now()
-                });
-
-                document.getElementById('message').value = '';
-                message.value = '';
-            }
+        'click div#send': function (event) {
+            messagePosted();
         },
 
-        'click div#smile' : function (event) {
+        'click div#smile': function (event) {
             if (Meteor.user())
                 name = Meteor.user().profile.name;
             else
@@ -55,8 +75,9 @@ if (Meteor.isClient) {
             var message = document.getElementById('message');
             Messages.insert({
                 name: name,
-                message: ':)',
-                time: Date.now()
+                message: '<emoji class="funny" style="height:32px;width: 32px;" />',
+                time: Date.now(),
+                from: "c"
             });
 
             document.getElementById('message').value = '';
---
+++ server/main.js
@@ -0,0 +1,33 @@
+/**
+ * Created by rajanchandi on 10/13/15.
+ */
+if (Meteor.isServer) {
+
+    Meteor.methods({
+        'removeAllMessages': function () {
+            return Messages.remove({});
+        },
+
+        'clientMessage': function(msg) {
+            Messages.insert({
+                name: 'Smilo',
+                message: 'Answer to...' + msg,
+                time: Date.now(),
+                from: "s"
+            });
+        }
+    });
+
+    /*Template.input.events = {
+        'keydown input#message': function (event) {
+            if (event.which == 13) {
+                Messages.insert({
+                    name: name,
+                    message: 'Message from the Server',
+                    time: Date.now(),
+                    from: "s"
+                });
+            }
+        }
+    };*/
+}
